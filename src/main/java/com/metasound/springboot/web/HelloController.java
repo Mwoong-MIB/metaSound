0d142dfa7831845815c5295e9701f4e904b6abf8
noisy full wagon <showmustgoon0702@gmail.com>
2021-11-17 01:49:03 +0900

    *error -> TestApp_build_failed : Test (Expect JUnit ver issue)
    + will repairing due 17.nov.21;
      : maybe it didn't work because of syntax error. => need check again

---
+++ src/main/java/com/metasound/springboot/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.metasound.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//컨트롤러를 JSON을 반환하는 컨트롤러로.
+//각 메소드마다 선언하던 ResponseBody를 한번에 사용하는 기능.
+@RestController
+public class HelloController {
+
+    @GetMapping("/Hello") //HTTP Method인 Get의 요청을 받을 수 있는 API 생성.
+    public String hello(){
+        return "Hello";
+    }
+
+}
---
+++ src/test/java/com/metasound/springboot/web/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.metasound.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//테스트를 진행할 때 JUnit에 내장된 실행자 SpringExtension 실행자 실행.
+//스프링 부트 테스트와 JUnit사이의 연결자 역할.
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    //스프링이 관리하는 bean을 주입.
+    @Autowired
+    //웹 API를 테스트할 때 사용.
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        //MockMvc 통해 /hello 주소로 HTTP GET 요청.
+        mvc.perform(get("/hello"))
+                //mvc.perform 결과를 검증. (200인지 아닌지)
+                .andExpect(status().isOk())
+                //mvc.perform 결과를 검증. (controller에서 hello를 리턴, 이 값이 맞는지)
+                .andExpect(content().string(hello));
+    }
+}
