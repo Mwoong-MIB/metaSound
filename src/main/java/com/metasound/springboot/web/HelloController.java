1616d15cb548e9b8f818723eb2290f8a63dfbbf4
noisy full wagon <showmustgoon0702@gmail.com>
2021-11-30 02:59:09 +0900

    * HelloDto make => UnitTest was succesful.

--- src/main/java/com/metasound/springboot/web/HelloController.java
+++ src/main/java/com/metasound/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.metasound.springboot.web;
 
+import com.metasound.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,6 +14,11 @@ public class HelloController {
     @GetMapping("/hello") //HTTP Method인 Get의 요청을 받을 수 있는 API 생성.
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 
 }
--- src/test/java/com/metasound/springboot/web/HelloControllerTest.java
+++ src/test/java/com/metasound/springboot/web/HelloControllerTest.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //테스트를 진행할 때 JUnit에 내장된 실행자 SpringExtension 실행자 실행.
 //스프링 부트 테스트와 JUnit사이의 연결자 역할.
@@ -31,5 +32,22 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 //mvc.perform 결과를 검증. (controller에서 hello를 리턴, 이 값이 맞는지)
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name", is(name)))
+                    .andExpect(jsonPath("$.amount", is(amount)));
+
     }
 }
